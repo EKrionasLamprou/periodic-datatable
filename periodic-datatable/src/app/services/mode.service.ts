a9167f03aeec1b26b69a5789ce13d3f1db699a71
Hector Krionas - Lamprou <e.krionas.lamprou@gmail.com>
2022-04-23 13:17:57 +0300

    Changes to chemical element colours in State mode.

--- periodic-datatable/src/app/services/mode.service.ts
+++ periodic-datatable/src/app/services/mode.service.ts
@@ -15,7 +15,7 @@ export class ModeService extends Observable {
   }
 
   /** Represents the current selected mode. Normal by default. */
-  private mode: Mode = Mode.Element
+  private mode: Mode = Mode.States
 
   /**
    * Gets the current {@linkcode Mode} of the periodic table.
