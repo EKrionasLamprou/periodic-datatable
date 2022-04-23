1edec911bab889e98d9900d541225ba2bdf3e4e1
Hector Krionas - Lamprou <e.krionas.lamprou@gmail.com>
2022-04-23 12:23:26 +0300

    Thermostat slider sass.

--- periodic-datatable/src/app/components/thermostat/thermostat.component.ts
+++ periodic-datatable/src/app/components/thermostat/thermostat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Mode } from 'src/app/enums/mode.enum'
+import { ModeService } from 'src/app/services/mode.service'
 import { ThermostatService } from 'src/app/services/thermostat.service'
 
 @Component({
@@ -8,11 +10,24 @@ import { ThermostatService } from 'src/app/services/thermostat.service'
 })
 export class ThermostatComponent implements OnInit {
 
-  constructor(private thermostatService: ThermostatService) { }
+  constructor(
+    private thermostatService: ThermostatService,
+    private modeService: ModeService) {
+  }
 
   ngOnInit(): void {
   }
 
+  /**
+   * Sets the value of the global temperature.
+   * @param value The new temperature value.
+   */
   public onTemperatureChange = (value: string) =>
     this.thermostatService.temperature = parseFloat(value)
+  
+  /**
+   * Returns true if the current mode is 'States', otherwise false.
+   */
+  public getVisibilityClass = (): string =>
+    this.modeService.getMode() === Mode.States ? '' : 'hidden'
 }
