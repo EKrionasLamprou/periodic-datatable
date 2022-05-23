1b768f7c7f5a7d9b623fd26e13db9d60d9dc2d1c
Hector Krionas - Lamprou <e.krionas.lamprou@gmail.com>
2022-05-23 23:19:11 +0300

    Connected datatable and periodic table elements.

--- periodic-datatable/src/app/components/datatable/datatable.component.ts
+++ periodic-datatable/src/app/components/datatable/datatable.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { elementAt } from 'rxjs';
 import { ChemicalElement } from 'src/app/models/chemical-element.model'
+import { ElementalComponent } from 'src/app/models/elemental-component';
 import { InvalidClassification } from 'src/app/models/error.model'
 import { ChemicalElementService } from 'src/app/services/chemical-element.service'
+import { ElementSelector } from 'src/app/services/element-selector.service';
 
 /**
  * Represents a datatable with information about the chemical elements.
@@ -11,8 +14,11 @@ import { ChemicalElementService } from 'src/app/services/chemical-element.servic
   templateUrl: './datatable.component.html',
   styleUrls: ['./datatable.component.sass']
 })
-export class DatatableComponent {
-  constructor(private chemicalElementService: ChemicalElementService) {
+export class DatatableComponent extends ElementalComponent {
+  constructor(
+    private chemicalElementService: ChemicalElementService,
+    elementSelector: ElementSelector) {
+    super(elementSelector)
   }
 
   /**
@@ -31,9 +37,35 @@ export class DatatableComponent {
     const classificationName: string | undefined =
       this.chemicalElementService.getClassification(classification)
 
-    if (!classificationName){
+    if (!classificationName) {
       throw new InvalidClassification(classification)
     }
     return classificationName
   }
+
+  /**
+   * Sets the given element as selected.
+   * @param element The chemical element to be set as selected.
+   */
+  public selectElement = (element: ChemicalElement): void => {
+    this.element = element
+    this.select()
+  }
+
+  /**
+   * Gets the html for a given chemical element's row on the datatable.
+   * @param element The element that corresponds to a datatable row.
+   * @returns A string of value 'selected' if the given element is selected,
+   * empty string otherwise.
+   */
+  public getRowHtmlClass = (element: ChemicalElement): string =>
+    this.isSelected(element) ? 'selected' : ''
+
+  /**
+   * Returns true if the given chemical is selected.
+   * @param element The given chemical element.
+   * @returns True if the given element is selected, false otherwise.
+   */
+  private isSelected = (element: ChemicalElement): boolean =>
+    this.elementSelector.selectedElement == element
 }
